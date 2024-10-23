c30735f3fc963e6c02fd181f1da1ea7179f27317
ARBABAL0 <arbaba214@gmail.com>
2024-10-24 00:32:52 +0500

    first commit

---
+++ src/pages/aboutus.tsx
@@ -0,0 +1,17 @@
+export default function Aboutus () {
+    return (
+        <div>
+      <h1 className="text-blue-500 font-bold"> This is an about us page </h1>
+          <p className="text-black-500"> 
+            Welocme to GoldenStitch Nation, Your destination for beautifully
+           crafted embroidered flags that celebrate the pride and heritage of countries around the world.
+           Each flags is meticulously designed with attentionto detaile, offering a unique, high-quility
+           represention of your nation. Whether you're looking to showcase yourpatriotism, honor your root
+           or gift someone special, Our collection has the perfectflag for you.Join is in celebrating global 
+           diversity, one stitch at a time. 
+
+                   
+           </p>
+        </div>
+      )
+  }
---
+++ src/pages/comments.tsx
@@ -0,0 +1,26 @@
+/*There are 4 types of comments in Next js
+
+single line comment: //Next JS
+Multi line comments: /* Next JS 
+
+
+Commenting as JSX:
+Commenting out of code:*/
+
+
+
+
+
+export default function Comments() {
+    return (
+        <div>
+            <h1 className="text-purple-500"> Comments in Next Js </h1>
+            <p>
+             Hello Companios this is Arbab Ali and you can contact for any kind of issues.
+
+            </p>
+            {/*This is a JSX comment.*/}
+        </div>
+        
+    );
+}
